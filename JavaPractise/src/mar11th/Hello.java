ba6a1152d75262300642a590def892020b80b1ab
seleniumriyaz <selenium.riyaz.shared@gmail.com>
2023-03-11 20:26:26 +0530

    Mar 11th Java Programs

---
+++ JavaPractise/src/mar11th/Hello.java
@@ -0,0 +1,11 @@
+package mar11th;
+
+public class Hello {
+	
+ public static void main(String[] args) {
+	
+	 System.out.println("Hello World");
+	 
+}
+
+}
---
+++ JavaPractise/src/mar11th/VariablesDemo3.java
@@ -0,0 +1,13 @@
+package mar11th;
+
+public class VariablesDemo3 {
+
+	static int z=30;
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
