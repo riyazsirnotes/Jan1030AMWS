528db6d318a14654635941094f9ee9aafe6216d7
seleniumriyaz <selenium.riyaz.shared@gmail.com>
2023-03-21 11:26:55 +0530

    Mar 21st programs

---
+++ JavaPractise/src/mar21st/SumTest.java
@@ -0,0 +1,21 @@
+package mar21st;
+
+public class SumTest {
+
+	public static void main(String[] args) {
+	
+		Sum obj1=new Sum();
+		
+		obj1.add(10, 20);
+		
+		obj1.add(10.22, 20.33);
+		
+		obj1.add(1100022000l, 3300022000l);
+		
+		obj1.add(10, 20,30);
+		
+		obj1.add(10.22, 20.33, 30.44);
+
+	}
+
+}
