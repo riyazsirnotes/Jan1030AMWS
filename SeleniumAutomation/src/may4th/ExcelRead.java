5384b19753aa97b5088508dc68d9a2ccde7e5a54
seleniumriyaz <selenium.riyaz.shared@gmail.com>
2023-05-04 14:07:49 +0530

    May 4th Programs

---
+++ SeleniumAutomation/src/may4th/ExcelRead.java
@@ -0,0 +1,63 @@
+package may4th;
+
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ExcelRead {
+
+	public static void main(String[] args) throws IOException {
+		
+		FileInputStream fis= new FileInputStream("D:\\001 Live Tech\\Book1.xlsx");
+		
+		XSSFWorkbook wb=new XSSFWorkbook(fis);
+		
+		XSSFSheet ws=wb.getSheet("Sheet");
+		
+		Iterator<Row>  rows=ws.iterator();
+		
+		Row row=null;
+		Cell cell=null;
+		
+		while(rows.hasNext())
+		{
+			row=rows.next();
+			
+			Iterator<Cell>  cells=row.iterator();
+			
+			while(cells.hasNext())
+			{
+				cell=cells.next();
+				
+				if(cell.getCellType()==CellType.STRING)
+				{
+					System.out.println(cell.getStringCellValue());
+				}
+				else if(cell.getCellType()==CellType.NUMERIC)
+				{
+					System.out.println(cell.getNumericCellValue());
+				}
+				else if(cell.getCellType()==CellType.BOOLEAN)
+				{
+					System.out.println(cell.getBooleanCellValue());
+				}
+			}
+		}
+		
+		FileOutputStream fos= new FileOutputStream("D:\\001 Live Tech\\Book1.xlsx");
+		wb.write(fos);
+		wb.close();
+		
+		System.out.println("End of Program");
+		
+	}
+}
